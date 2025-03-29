7bc5c56c39e6061b682e39fe6944adb70ded873a
Pierretgv <79547031+Pierretgv@users.noreply.github.com>
2025-03-29 22:36:47 +0100

    Dodano liste produktow dla refrigeratedcontainer i poszerzono testowanie w mainie

--- Containers/RefrigeratedContainer.cs
+++ Containers/RefrigeratedContainer.cs
@@ -1,15 +1,58 @@
+using System;
+using System.Collections.Generic;
+
 namespace ContainerManagement.Containers
 {
     public class RefrigeratedContainer : Container
     {
         public string ProductType { get; }
         public double Temperature { get; }
+        
+        private static readonly Dictionary<string, double> ProductTemperatures = new()
+        {
+            { "Banany", 13 },
+            { "Jabłka", 1 },
+            { "Mleko", 4 },
+            { "Wołowina", -2 },
+            { "Ryby", -18 },
+            { "Lody", -20 },
+            { "Szczepionki", -70 },
+            { "Ser", 3 },
+            { "Warzywa", 5 },
+            { "Czekolada", 18 }
+        };
 
         public RefrigeratedContainer(double maxCapacity, double height, double ownWeight, double depth, string productType, double temperature)
             : base(maxCapacity, height, ownWeight, depth, "C")
         {
+            if (!ProductTemperatures.ContainsKey(productType))
+                throw new ArgumentException($"Nieobsługiwany produkt: {productType}");
+
+            if (temperature > ProductTemperatures[productType])
+                throw new ArgumentException($"Temperatura dla {productType} nie może być wyższa niż {ProductTemperatures[productType]}°C!");
+
             ProductType = productType;
             Temperature = temperature;
+        }
+
+        public override string ToString()
+        {
+            return $"[{SerialNumber}] Produkt: {ProductType}, Temperatura: {Temperature}°C, Waga ładunku: {LoadWeight}/{MaxCapacity} kg";
+        }
+        
+        public static void PrintProductTable()
+        {
+            Console.WriteLine("Lista obsługiwanych produktów chłodniczych:");
+            Console.WriteLine("+----------------+----------------------+");
+            Console.WriteLine("| Produkt       | Minimalna temp. (°C)  |");
+            Console.WriteLine("+----------------+----------------------+");
+
+            foreach (var product in ProductTemperatures)
+            {
+                Console.WriteLine($"| {product.Key,-12} | {product.Value,18}°C |");
+            }
+
+            Console.WriteLine("+----------------+----------------------+");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,48 @@
-﻿using ContainerManagement.Containers;
+﻿using System;
+using System.Collections.Generic;
+using ContainerManagement.Containers;
 using ContainerManagement.Transport;
+
 class Program
 {
     static void Main()
     {
-        var ship = new Ship(10, 200, 30);
+        RefrigeratedContainer.PrintProductTable();
 
-        var bananaContainer = new RefrigeratedContainer(1000, 250, 500, 600, "Bananas", 13.3);
-        var gasContainer = new GasContainer(500, 200, 300, 500, 5);
-        var milkContainer = new LiquidContainer(1000, 250, 500, 600, false);
-        
-        bananaContainer.Load(900); 
-        gasContainer.Load(450);      
-        milkContainer.Load(500);  
-        
-        ship.LoadContainer(bananaContainer);
-        ship.LoadContainer(gasContainer);
-        ship.LoadContainer(milkContainer);
-        
-        ship.PrintCargoDetails();
+        var ship = new Ship(18, 200000, 30);
 
-        
-        Console.WriteLine(ship.IsOverloaded() ? "Statek jest przeciążony!" : "Statek gotowy do rejsu.");
+        var container1 = new RefrigeratedContainer(1000, 250, 500, 600, "Banany", 13);
+        var container2 = new RefrigeratedContainer(1000, 250, 500, 600, "Jabłka", 1);
+        var container3 = new RefrigeratedContainer(1000, 250, 500, 600, "Mleko", 4);
+        var container4 = new RefrigeratedContainer(1000, 250, 500, 600, "Wołowina", -2);
+        var container5 = new RefrigeratedContainer(1000, 250, 500, 600, "Ryby", -18);
+        var container6 = new RefrigeratedContainer(1000, 250, 500, 600, "Lody", -20);
+        var container7 = new RefrigeratedContainer(1000, 250, 500, 600, "Szczepionki", -70);
+        var container8 = new RefrigeratedContainer(1000, 250, 500, 600, "Ser", 3);
+        var container9 = new RefrigeratedContainer(1000, 250, 500, 600, "Warzywa", 5);
+        var container10 = new RefrigeratedContainer(1000, 250, 500, 600, "Czekolada", 18);
 
-        Console.WriteLine("Naciśnij Enter, aby zamknąć...");
-        Console.ReadLine();
+        var liquidContainer1 = new LiquidContainer(1000, 250, 500, 600, false);
+        var liquidContainer2 = new LiquidContainer(1000, 250, 500, 600, true);
+
+        var gasContainer1 = new GasContainer(500, 200, 300, 500, 5);
+        var gasContainer2 = new GasContainer(500, 200, 300, 500, 8);
+
+        ship.LoadContainer(container1);
+        ship.LoadContainer(container2);
+        ship.LoadContainer(container3);
+        ship.LoadContainer(container4);
+        ship.LoadContainer(container5);
+        ship.LoadContainer(container6);
+        ship.LoadContainer(container7);
+        ship.LoadContainer(container8);
+        ship.LoadContainer(container9);
+        ship.LoadContainer(container10);
+        ship.LoadContainer(liquidContainer1);
+        ship.LoadContainer(liquidContainer2);
+        ship.LoadContainer(gasContainer1);
+        ship.LoadContainer(gasContainer2);
+
+        Console.WriteLine(ship);
     }
 }
